8dc0186964267029cac012c3470d2151ca5a996d
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-03-11 15:30:39 +0000

    Update run_asap2.sh

--- asap2/bin/run_asap2.sh
+++ asap2/bin/run_asap2.sh
@@ -2,4 +2,4 @@
 
 cd asap2/
 
-python -u ../bin/asap2.py -i input -c silva-138-99-nb-weighted-classifier.qza -p 10 -o maleth
+python -u ../bin/asap2.py -i data -c silva-138-99-nb-weighted-classifier.qza -p 10 -o maleth
